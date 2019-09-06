9ba5b07f1fb61a0a1890cff03b13e6038fc19d07
Andrey Milanovich <milanovichandrey@tut.by>
2019-09-06 11:36:09 +0300

    add pycharm folder in git ignore

---
+++ telegrambot.py
@@ -0,0 +1,24 @@
+#!/usr/lib/python3
+# -*-coding:utf-8 -*-
+
+
+import telebot
+
+bot = telebot.TeleBot('954668606:AAER9Kv9qxd841n2FJrcpF8fgJCRLQeyscU')
+
+
+class skoda_bot:
+
+    @bot.message_handler(content_types=['text'])
+
+    def get_text_messages(message):
+        if message.text == "Skoda":
+            bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+        elif message.text == "/help":
+            bot.send_message(message.from_user.id, "Напиши привет")
+        else:
+            bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling(none_stop=True, interval=0)
+
